db84ab43820498ec08b4f29152cd6183d5b56990
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2019-11-11 19:47:16 -0500

    Update root path to go to the welcomes#index
    This commit updates the routes file to send the root path to the
    welcomes#index action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :welcomes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcomes#index'
 end
